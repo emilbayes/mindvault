83b6be92df4392f90a9c3ac1dbd97d2db72a2cf5
Emil Bay <github@tixz.dk>
2017-08-09 08:39:18 +0200

    Change to sodium-native, add bells and whistles

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 'use strict'
 
-var assert = require('assert')
-var sodium = require('sodium').api
+var assert = require('nanoassert')
+var sodium = require('sodium-native')
 
-module.exports = function (passphrase, salt, appId) {
-  appId = appId || Buffer.alloc(32)
-  assert.ok(Buffer.isBuffer(passphrase), 'passphrase must be Buffer')
-  assert.ok(Buffer.isBuffer(salt), 'salt must be Buffer')
-  assert.ok(Buffer.isBuffer(appId), 'appId must be Buffer')
-  assert.ok(appId.length === module.exports.APP_ID_BYTES, 'appId must be APP_ID_BYTES long (' + module.exports.APP_ID_BYTES + ')')
+module.exports = hash
+module.exports.APP_ID_BYTES = sodium.crypto_generichash_KEYBYTES
+module.exports.BYTES = sodium.crypto_box_SEEDBYTES
 
-  var saltHash = sodium.crypto_generichash(
-    sodium.crypto_pwhash_SALTBYTES,
+function hash (out, passphrase, salt, appId, opts) {
+  /* passphrase, sat, appId, [opts] */
+  if (!Buffer.isBuffer(appId) && opts === null) {
+    opts = appId
+    appId = salt
+    salt = passphrase
+    passphrase = out
+    out = Buffer.allocUnsafe(module.exports.BYTES)
+  }
+
+  assert(Buffer.isBuffer(out), 'out must be Buffer')
+  assert(Buffer.isBuffer(passphrase), 'passphrase must be Buffer')
+  assert(Buffer.isBuffer(salt), 'salt must be Buffer')
+
+  // Init app id
+  appId = appId || Buffer.alloc(module.exports.APP_ID_BYTES)
+  assert(Buffer.isBuffer(appId), 'appId must be Buffer')
+  assert(appId.length === module.exports.APP_ID_BYTES, 'appId must be APP_ID_BYTES long (' + module.exports.APP_ID_BYTES + ')')
+
+  // Init opts
+  opts = opts || {}
+
+  if (opts.memlimit == null) opts.memlimit = sodium.crypto_pwhash_MEMLIMIT_SENSITIVE
+  assert(typeof opts.memlimit === 'number', 'opts.memlimit must be number')
+  assert(opts.memlimit >= sodium.crypto_pwhash_MEMLIMIT_MIN, 'opts.memlimit must be at least (' + sodium.crypto_pwhash_MEMLIMIT_MIN + ')')
+  assert(opts.memlimit <= sodium.crypto_pwhash_MEMLIMIT_MAX, 'opts.memlimit must be at least (' + sodium.crypto_pwhash_MEMLIMIT_MAX + ')')
+
+  if (opts.opslimit == null) opts.opslimit = sodium.crypto_pwhash_OPSLIMIT_SENSITIVE
+  assert(typeof opts.opslimit === 'number', 'opts.opslimit must be number')
+  assert(opts.opslimit >= sodium.crypto_pwhash_OPSLIMIT_MIN, 'opts.opslimit must be at least (' + sodium.crypto_pwhash_OPSLIMIT_MIN + ')')
+  assert(opts.opslimit <= sodium.crypto_pwhash_OPSLIMIT_MAX, 'opts.opslimit must be at least (' + sodium.crypto_pwhash_OPSLIMIT_MAX + ')')
+
+
+  var saltHash = Buffer.allocUnsafe(sodium.crypto_pwhash_SALTBYTES)
+  sodium.crypto_generichash(
+    saltHash,
     salt,
     appId
   )
 
-  if (saltHash === undefined) throw new Error('Salt hashing was unsuccessful')
-
-  var pwHash = Buffer.allocUnsafe(sodium.crypto_box_SEEDBYTES)
-  var pwhashRes = sodium.crypto_pwhash(
-    pwHash,
+  sodium.crypto_pwhash(
+    out,
     passphrase,
     saltHash,
-    sodium.crypto_pwhash_OPSLIMIT_SENSITIVE,
-    sodium.crypto_pwhash_MEMLIMIT_SENSITIVE,
+    opts.opslimit,
+    opts.memlimit,
     sodium.crypto_pwhash_ALG_DEFAULT
   )
 
-  if (pwhashRes === undefined) throw new Error('Passphrase hashing was unsuccessful')
-
-  return pwHash
+  return out
 }
-
-module.exports.APP_ID_BYTES = sodium.crypto_generichash_KEYBYTES
